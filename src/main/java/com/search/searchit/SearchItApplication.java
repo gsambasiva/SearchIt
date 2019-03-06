180e3052f4f5464cb02cf6d98a90d4a9abc4bf0a
gsambasiva <sambasivarao.gadipudi@infor.com>
2019-03-06 18:30:35 +0530

    Initial commit for SearchIt

---
+++ src/main/java/com/search/searchit/SearchItApplication.java
@@ -0,0 +1,13 @@
+package com.search.searchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchItApplication.class, args);
+	}
+
+}
